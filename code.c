9f41fb48e283e20ec3aaa70b3051699434eb8f16
TARUN SHANKAR NARAIN <37019970+tarunnarain@users.noreply.github.com>
2018-12-28 00:28:07 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,82 +1,132 @@
 #include<reg51.h>
-sbit rd = P2^5; 
-sbit wr = P2^6;
-sbit intr = P2^7;
-sbit l = P2^0;
-sbit s = P2^1;
-void delay()
-{
- TR0=1;
- {
- while(TF0==0);
- TF0=0;
- }
- TR0=0;
 
- }
+#define MID_NIGHT 0 					//12 A.M
+#define MORNING 5					//5	 A.M
+#define	EVENING	(18 + 6)				//18 = 6 P.M, IN BCD 18 IS (0001 1000) I.E = 24 
+#define TRAFFIC_THRESOLD 0x0F				//TRAFFIC THRESHOLD LEVEL
 
-void delay1()
-{
-unsigned int x;
-for(x=0;x<6530;x++);
+#define SET 1
+#define CLEAR 0
+
+
+sbit adc_rd = P2^5;					// READ PIN CONNECTED TO ADC
+sbit adc_wr = P2^6;					// WRITE PIN CONNECTED TO ADC
+sbit intr = P2^7;					// ADC INTERRUPT PIN
+sbit main_latch = P2^1;					// PIN CONNECTED TO MAIN LATCH TURNNING LIGHTS ON/OFF AS PER TIME
+sbit volt_latch = P2^0;					// PIN CONNECTED TO VOLTAGE SWITCHING LATCH HIGH/LOW
+
+
+
+char count,sec,min,hr;
+
+void init()						//funtion for initialization of the systems
+{	
+	TMOD=0X01;					//timer 0 mode 1, 16 bits				
+	TH0=15574>>8;					
+	TL0=15574;
+	IE=0x82;					//Interrupt Enable
+	TR0=1;						//Timer0 Starts now
 }
 
-void adc()
+void mlatch_controller()				//Main Latch controlling function
 {
-  rd=1;
-	wr=0;
-	delay1();
-	wr=1;
-	while(intr==1);
-	rd=0;
-  }
+	if(hr >= EVENING && hr < MORNING)
+	{
+			main_latch=SET;			//Lights ON 
+	}
+	else
+	{
+			main_latch=CLEAR;		//Lights OFF
+	}	
+}
+
+void time_7seg_bcd()					//setting relation between sec,min,hr and converting hr:min:sec in BCD format
+{
+	if(sec==10|sec==26|sec==42|sec==58|sec==74)	//when seconds past = 10,20,30,40,50 
+		sec+=6;
+	else if(sec==90)														//when seconds past = 60
+	{
+		sec=0;
+		min++;
+	}
+	
+	if(min==10|min==26|min==42|min==58|min==74|min==90)	//when mins past = 10,20,30,40,50
+		min+=6;
+	else if(min==95)			//when mins past = 60
+	{
+		min=0;
+		hr++;
+		mlatch_controller();
+	}
+	
+	if(hr==10|hr==26)			//when hr past is 10,20
+		hr=hr+6;
+	else if(hr==35)				//when hr past is 24
+		hr=0;
+
+}
+
+void pulse()
+{
+	unsigned int x=65535;
+	while(x--);
+	
+}
+
+void timmer0_isr() interrupt 1			 //Timer0 Interrupt invoked after every 50000us ie 50ms
+{															
+		
+		count++;										
+		if(count%20==0)			//when Interrupt has been invoked 20 time i.e when 50ms*20 = 1000ms has been passed  
+		{
+			sec++;		
+			count=0;
+			time_7seg_bcd();	//calling fun to process and display time in bcd after each and every second
+		}
+/*Calculation 35566 - 50000 + 32 + 2 + 2 + 2 = 15574*/
+	TH0=15574<<8;								 								TL0=15574;		
+}
+
+char adc_conv()					//ADC converter function
+{
+	unsigned char temp;
+	adc_rd=1;
+	adc_wr=0;
+	pulse();
+	adc_wr=1;				//converstion strats here
+	while(intr==1);		
+	adc_rd=0;				//value loaded in ADC_data lines
+	temp=P1;				//storing u.c pin_data		
+	
+	return temp;				//returing the value	
+}
+
+void vlatch_controller(unsigned char dat)	//fun. to control the rellay
+{								
+		if(dat> TRAFFIC_THRESOLD)																
+		{
+			volt_latch=SET;		//glow street lights at high voltage
+		}
+		else 
+		{
+			volt_latch=CLEAR;	//glow street lights at low voltage
+		}
+}
+
+
 
 void main()
 {
-unsigned int a=0,b=0,c=0,i,j,k;
-	P0=0;P3=0;
-  TMOD=0X01;
-  TL0=0;
-  TH0=0;
-while(1)
-{
+	unsigned char adc_data;
 
+	init();
 
-for(i=0;i<24;i++)
-    {
-	    for(j=0;j<60;j++)
-	    {
-	    	for(k=0;k<60;k++)
-	    	{
-	    	if(a==9|a==25|a==41|a==57|a==73|a==89)a=a+6;
-	    	delay();
-	    	a++;	
-	    	if(a==96)a=0;
-      	if(c>=0&c<5)
-	    	{	s=1;
-	    		adc();
-	    		if(P1<0xff)
-	    		{l=0; }
-	    		else
-	    		{l=1;}
-	    		}
-			else if(c>18|c<6) {s=1;l=1;}
-		  else s=0;
-		   }
-	  	if(b==9|b==25|b==41|b==57|b==73|b==89)b=b+6;
-			
-	    	b++;
-	    	if(b==96)b=0;
-	    	P3=b;
-		    }
-		
-		if(c==9|b==25|b==41)c=c+6;
-		
-		c++;
-		if(c==36)c=0;
-		P0=c;
-		}	 		   	
-
-
-}							 
+	while(1)
+	{
+		P0=hr;
+		P3=min;
+		adc_data=adc_conv();
+		if(main_latch==SET)				//if street lights are on, call voltage controller
+			vlatch_controller(adc_data);
+	}		
 }
